9ef6694e8c48391a30f6ec2718cae0eb58c152cd
Volodymyr I Pankiv <vpankiv@softserveinc.com>
2024-11-13 16:48:20 +0200

    topic1

---
+++ index.js
@@ -0,0 +1,229 @@
+// console.log(123) // comment1
+
+// comment
+
+
+// змінні
+
+// (let const) var
+
+// let message = "hello";
+
+// console.log(message);
+
+
+// let message = "hello";
+
+// message = "world";
+
+// const message = "hello";
+
+// message = "world";
+
+
+
+// console.log(message);
+
+// типи даних
+
+// string - "sdgdgdfhfdh325u83y523"   'sfsgdsfgf'
+
+// number - 5875757
+
+// boolean - true false
+
+// undefined - 
+
+// let message;
+
+// console.log(message);
+
+// null
+
+// let message = 2424;
+
+// message = null;
+
+
+// object
+
+// let user = {
+//     name: "Volodymyr",
+//     age: 30,
+// }
+
+// bigInt
+
+// 353895395n
+
+// динамічно-типізована мова
+
+// let number = 3000;
+
+// number = "string";
+
+// console.log(number);
+
+// let number = 3000;
+
+// number = "sagdggd";
+
+// console.log(typeof number);
+
+// alert("hello");
+
+// prompt("hello");
+// let name = prompt("hello whats your name?");
+
+// // alert(name);
+// alert(`Hi ${name}`);
+
+// let ok = confirm("Are you ok?");
+
+// console.log(ok);
+
+
+// * + - / %
+
+// console.log(2 + 2)
+// console.log(2 * 2)
+// console.log(2 - 2)
+// console.log(2 / 2)
+// console.log(-100 / 0);
+
+
+// let number = 10;
+
+// number = number + 2;
+// number = number * 2;
+// number += 2;
+// number *= 2;
+
+// 0.1 + 0.2 = 0.3  0.3000000004
+
+// console.log(0.1 + 0.3);
+
+// let number = 10;
+
+// // let num2 = number++;
+// let num2 = ++number;
+
+
+// console.log(number);
+// console.log(num2);
+
+// number += 1;
+// number++;
+// ++number
+// number--;
+
+// let str1 = "hello";
+
+// let str2 = "world";
+
+
+// console.log(str1 + str2);
+// console.log("volo" + "dymyr");
+
+
+// console.log("text" + 2);
+// console.log("text" + true);
+// console.log("2" + 2);
+
+// console.log("4" / 2);
+// console.log("efeg" / 2);
+// Not a Number - помилка обрахунку NaN
+
+// console.log(2 + 2 + "2"); // "42"
+
+// console.log(10 == 10); // == ===
+// console.log(10 == 11); // == ===
+
+// 10 >= 6;
+// 10 <= 11;
+
+// 10 > 11
+// 11 < 10
+
+// console.log(0 == "");
+// console.log(0 === 0);
+
+// console.log(10 !== 11);
+
+// console.log(10 > 6 && 10 < 11);
+// console.log(10 < 6 || 10 > 11);
+
+// console.log(Number("dfdf"));
+// console.log(parseInt("2424.3434"));
+// console.log(parseFloat("2f424.343sfdgd"));
+
+
+// if 
+
+// switch
+
+// ternary operator
+
+// age = 25;
+
+// if (age > 18){
+//     console.log("Adult");
+// }
+
+// if (age > 18){
+//     console.log("Adult");
+// } else {
+//     console.log("Not adult");
+// }
+
+
+// if (age > 18){
+//     console.log("Adult");
+// } else if(age < 18) {
+//     console.log("Not adult");
+// }else{
+//     console.log("you are 18")
+// }
+
+// if (age > 20 && age <=30){
+//     console.log("age 20-30");
+// } else if(age < 18) {
+//     console.log("Not adult");
+// }else{
+//     console.log("you are 18")
+// }
+
+// age = 25;
+
+// switch(age){
+//     case 25:
+//         console.log("you are 25");
+//         break;
+//     case 30:
+//         console.log("you are 30");
+//         break;
+//     default:
+//         console.log("you are not 25 and not 30");
+
+// }
+
+// age = 20;
+
+
+// let result = (age > 18) ? "adult" : "not adult";
+
+// console.log(result);
+
+// if (age > 18){
+//     console.log("Adult");
+// } else {
+//     console.log("Not adult");
+// }
+
+
+
+
+
+
+
+
