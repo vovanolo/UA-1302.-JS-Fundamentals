0cb2b4b0046009eaa71610c41b9e84dd09cee9f8
Volodymyr I Pankiv <vpankiv@softserveinc.com>
2024-12-09 18:35:32 +0200

    topic8

---
+++ topic8.js
@@ -0,0 +1,106 @@
+// 
+
+// var
+// const let
+// num1 num2
+// `My name is ${num1 + num2} hello
+// text
+// `
+// 'My name is ' + name + " hello" ;
+// promise async/await
+
+// 
+// spreat operator
+
+// ...
+
+// const num1 = [1,2,3,4,5,7];
+
+// const copynum = [...num1];
+
+// console.log(Math.min(...num1));
+
+
+// function sum(par1, ...elem){
+//     console.log(par1);
+//     console.log(elem);
+// }
+// sum("+",3,4,7,8,6);
+
+// desctructuring 
+// [] {}
+
+// const students = ["Ivan", "Petro", "Andriy"];
+
+// students[0];
+
+// let ivan = students[0];
+// const [student1, ...restStudents] = students;
+
+// console.log(student1);
+
+// console.log(restStudents);
+
+// const obj = {
+//     // name: "Volodymyr",
+//     age: 25,
+//     friends: ["friend1", 'friend2'],
+//     sonObj: {
+//         name:"name1",
+//         age: 55
+//     }
+// }
+
+// // obj.name + obj.age
+// const {name = "defaultName"} = obj;
+// // const [fr1,fr2] = friends;
+// console.log(name);
+// function func1(par1 = 10){
+//     console.log(par1);
+// }
+// func1();
+// console.log(age);
+// console.log(name);
+// console.log(fr1);
+
+
+// arrow functions
+
+// function sum() {
+//     console.log('sfsf');
+// }
+
+
+// const sum = function(a) {
+//     console.log('sfsf');
+// }
+
+// const sum = (a,b) => {
+//     console.log(a + b);
+// }
+
+// const sum = () => {
+//     return a + b;
+// }
+
+// const sum = (a,b) => a + b;
+// const sum = (a,b) =>{
+//     if(a > b){
+//         return a + b;
+//     }
+// };
+
+
+// sum(2,4)
+
+// [1,3,45].map(elem => elem + 2)
+//         .filter(elem => elem > 5);
+
+// const recipeMap = new Map([
+//     ['cucumber', {name: "nam"}],
+//     ['tomatoes', 350],
+//     ['onion', 50]
+// ]);
+
+// console.log(recipeMap);
+    
