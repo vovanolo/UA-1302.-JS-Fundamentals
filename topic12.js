b6b0bc665fefe5b63881f79cd1e9522ab9e6f710
Volodymyr I Pankiv <vpankiv@softserveinc.com>
2024-12-18 18:23:25 +0200

    topic12

---
+++ topic12.js
@@ -0,0 +1,19 @@
+// let a:number = 20;
+// let b:string = "hello";
+// let c:any = "sd";
+// // a = 'sf';
+// console.log(a);
+// console.log(b);
+// function sum(a:number, b:number){
+//     console.log(a + b);
+// }
+// sum(10, 10);
+// function sum(a:number, b:number):void {
+//     // console.log(a + b);
+//     return a + b;
+// }
+function sum(a, b) {
+    // console.log(a + b);
+    return a + b;
+}
+console.log(sum(10, 10));
---
+++ topic12.ts
@@ -0,0 +1,46 @@
+// let a:number = 20;
+
+// let b:string = "hello";
+
+// let c:any = "sd";
+// // a = 'sf';
+// console.log(a);
+// console.log(b);
+
+
+// function sum(a:number, b:number){
+//     console.log(a + b);
+// }
+
+// sum(10, 10);
+
+// function sum(a:number, b:number):void {
+//     // console.log(a + b);
+//     return a + b;
+// }
+
+// function sum(a:number, b:number):number {
+//     // console.log(a + b);
+//     return a + b;
+// }
+
+// console.log(sum(10, 10))
+
+// sum()
+
+// let address: { street: string, house: number } = {
+//     street: "Franko",
+//     house: 22
+//    }
+
+// address.house = 20;
+
+// let a:number = 22;
+// let b:string = "abc";
+// a = b;
+
+// const ages: number[] = [18, 25, 30, 60];
+
+// let tuple1: [string, number, boolean] = ['student', "s", true];
+
+// const cities: string[] | number[] = ["Lviv", "Kyiv", "Rivne", "Dnipro"];
